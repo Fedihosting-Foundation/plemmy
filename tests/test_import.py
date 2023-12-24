eaf417c9bd95a41b8b4fd22bd129d129c25c7ff7
Travis Kessler <travis.j.kessler@gmail.com>
2023-12-24 18:02:31 -0500

    Remove unnecessary code

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,11 +2,8 @@ import pytest
 
 
 def test_import():
+
     try:
         import plemmy
     except ModuleNotFoundError as ex:
         pytest.fail(f"{ex}")
-
-
-if __name__ == "__main__":
-    test_import()
